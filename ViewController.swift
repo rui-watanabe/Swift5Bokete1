e6c6d792721b1676065809c673f751c886a7b512
rui-watanabe <wataru610806@gmail.com>
2020-07-10 22:33:29 +0900

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5Bokete1
+//
+//  Created by watar on 2020/07/10.
+//  Copyright © 2020 rui watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
