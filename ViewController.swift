063535cf5457a56b9992983b5da3218bb5bb801b
rui-watanabe <wataru610806@gmail.com>
2020-07-11 12:46:43 +0900

    set UI pars and set API

--- ViewController.swift
+++ ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Swift5Bokete1
 //
-//  Created by watar on 2020/07/10.
+//  Created by watar on 2020/07/11.
 //  Copyright © 2020 rui watanabe. All rights reserved.
 //
 
@@ -12,9 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
